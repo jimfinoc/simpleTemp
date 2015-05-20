b88c990571d39446c7f672dbd8f3d5156c943b99
J <jimfinoc@gmail.com>
2015-05-19 20:57:39 -0400

    #import request

--- takeTemp.py
+++ takeTemp.py
@@ -6,7 +6,7 @@ import sys
 import json
 import time
 import datetime
-import requests
+#import requests
 #from Adafruit_7SegmentPlus import SevenSegment
 #from optparse import OptionParser
 import myColorText
